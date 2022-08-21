464288ba61980dfe04e9c3f35c7014396ad5b406
markom132 <markomarkoviccb@gmail.com>
2022-08-21 20:32:58 +0200

    update service interfaces

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/Ownerservice.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/Ownerservice.java
@@ -2,14 +2,7 @@ package com.example.sfgpetclinic.services;
 
 import com.example.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface Ownerservice {
-    Owner findById(Long id);
-
+public interface Ownerservice extends CrudService<Owner, Long> {
     Owner findByLastname(String lastName);
 
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
